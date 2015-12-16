aa5263485fc42a4da64792db3fb2539a8b639f38
Andrey Dresvyannikov <andrey@dresvyannikov.ru>
2015-12-16 09:31:39 +0300

    add coverage generation while testing

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,5 @@ from __future__ import print_function, unicode_literals, absolute_import
 from fabric.api import local
 
 
-def test(verbose=False):
-    verb = '-v' if verbose else ''
-    local('python -m unittest discover -s tests'.format(verb))
+def test():
+    local('coverage run -m unittest discover -s tests')
