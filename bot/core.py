d9b27ede600b5407fed1b28eb1f1d441bca8ebc8
Andrey Dresvyannikov <andrey@dresvyannikov.ru>
2015-12-19 17:12:32 +0300

    add methods to parse incoming message and call proper function in ChatBot

--- bot/core.py
+++ bot/core.py
@@ -3,8 +3,22 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 class ChatBotCore:
     """ ChatBotCore class is base class to inherit from to create new bots """
-    def __init__(self):
-        pass
+    def __init__(self, name='ChatBot'):
+        self.name = name
+
+    def _parse_command_message(self, message):
+        components = message.split()
+        command = components[0][1:]
+        args = tuple(cmp for cmp in components[1:] if cmp)
+        return (command, args)
+
+    def _parse(self, user, message):
+        """ Parse incoming message and call proper method for further actions """
+        if message.startswith('/'):
+            command, args = self._parse_command_message(message)
+            return self._command(user, command, *args)
+        else:
+            return self._free_text(user, message)
 
     def notify(self, user, notification):
         """ Send notification to user
@@ -12,14 +26,17 @@ class ChatBotCore:
         """
         pass
 
-    def reply(self, user, message):
+    def _free_text(self, user, message):
         """ Send reply message to user
         send reply for user free text message (as opposed to command message)
         """
         pass
 
-    def process(self, user, message):
+    def _command(self, user, command, *args):
         """ Process user command message
         process user command message and optionally reply
         """
+        pass
+
+    def _send_message(self, user, message):
         pass
--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -1,4 +1,5 @@
 import unittest
+import mock
 
 from bot.core import ChatBotCore
 
@@ -7,13 +8,44 @@ class TestChatBot(unittest.TestCase):
     def setUp(self):
         self.chatbot = ChatBotCore()
 
+    def test_chatbot_name(self):
+        """ Bot can have name """
+        name = 'test_name'
+        cb = ChatBotCore(name)
+        self.assertEqual(name, cb.name)
+
+    def test__parse(self):
+        """ _parse method should return proper function and arguments for it
+        by default if message starts with / - it's command message
+        otherwise it's free text message
+        """
+        command = '/command'
+        self.chatbot._command = mock.Mock()
+        self.chatbot._parse(None, command)
+        self.chatbot._command.assert_called_once_with(None, command[1:])
+
+        free_text = 'message'
+        self.chatbot._free_text = mock.Mock()
+        self.chatbot._parse(None, free_text)
+        self.chatbot._free_text.assert_called_once_with(None, free_text)
+
+    def test__parse_command_message(self):
+        """ _parse_command_message should return command name and arguments for the command if required """
+        command = 'command'
+        cmd1 = '/{}'.format(command)
+        cmd2 = '/{} 1'.format(command)
+        cmd3 = '/{} 1   2 3'.format(command)
+        self.assertEqual((command, ()), self.chatbot._parse_command_message(cmd1))
+        self.assertEqual((command, ('1',)), self.chatbot._parse_command_message(cmd2))
+        self.assertEqual((command, ('1', '2', '3')), self.chatbot._parse_command_message(cmd3))
+
     def test_command_message(self):
         """ Bot should be able to respond to certain command messages """
-        self.assertTrue(hasattr(self.chatbot, 'reply'))
+        self.assertTrue(hasattr(self.chatbot, '_command'))
 
     def test_free_text_message(self):
         """ Bot should be able to respond to any free text message """
-        self.assertTrue(hasattr(self.chatbot, 'process'))
+        self.assertTrue(hasattr(self.chatbot, '_free_text'))
 
     def test_notification_message(self):
         """ Bot should be able to respond to any free text message """
