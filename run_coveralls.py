d918d4dedd5fc289578565126fc737f124a6e0e2
Andrey Dresvyannikov <andrey@dresvyannikov.ru>
2015-12-26 00:07:13 +0300

    fix run_coveralls to actually run coveralls instead of cover

--- run_coveralls.py
+++ run_coveralls.py
@@ -7,13 +7,5 @@ from subprocess import call
 
 if __name__ == '__main__':
     dot_coverage_path = os.path.join(os.getcwd(), '.coverage')
-    if os.path.isfile(dot_coverage_path):
-        os.remove('.coverage')
-
-    # create a report from the coverage data
-    if 'TRAVIS' in os.environ:
-        rc = call('coveralls')
-        raise SystemExit(rc)
-    else:
-        rc = call(['coverage', 'report'])
-        raise SystemExit(rc)
+    rc = call('coveralls')
+    raise SystemExit(rc)
